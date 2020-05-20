f2ba770b63138e7404e5474398dfd255921d9103
Mikkel Ricky <rimi@aarhus.dk>
2020-05-20 13:29:26 +0200

    OS2WEB-156: Cleaned up. Added @TODOs.

--- modules/loop_search_db/loop-search-db--form.tpl.php
+++ modules/loop_search_db/loop-search-db--form.tpl.php
@@ -14,58 +14,63 @@
         <?php print drupal_render_children($form); ?>
       </div>
 
-      <div class="search-filters">
-        <?php if (isset($form['#facets']['type'])): ?>
-          <div class="search-filters--type">
-            <?php foreach ($form['#facets']['type'] as $index => $facet): ?>
-              <?php $name = 'f[]'; ?>
-              <?php $text = trim($facet['filter'], '"'); ?>
-              <?php $value = $text ? urlencode('type').':'.$text : ''; ?>
-              <?php $checked = $facet['active'] ?? FALSE; ?>
-              <label class="search-filters--label <?php if ($checked) { print ' is-active '; } ?>"><?php print $facet['text'] ?? $facet['filter']; ?>
-                <input type="checkbox" name="<?php print $name; ?>" <?php if ($checked) { print ' checked '; } ?> value="<?php print htmlspecialchars($value) ?>"/>
-              </label>
-            <?php endforeach ?>
-          </div>
-        <?php endif ?>
+      <?php if (isset($form['#facets'])): ?>
+        <div class="search-filters">
+          <?php if (isset($form['#facets']['type'])): ?>
+            <div class="search-filters--type">
+              <?php // @TODO We should propably use some theme function for this, right? ?>
+              <?php foreach ($form['#facets']['type'] as $index => $facet): ?>
+                <?php $name = 'f[]'; ?>
+                <?php $text = trim($facet['filter'], '"'); ?>
+                <?php $value = $text ? urlencode('type') . ':' . $text : ''; ?>
+                <?php $checked = $facet['active'] ?? FALSE; ?>
 
-        <?php if (isset($form['#facets']['field_subject:name'])): ?>
-          <span class="search-filters--subject-wrapper">
-            <span class="search-filters--subject-wrapper-label">
-              <?php $active_filters = loop_search_db_get_active_filters('field_subject'); ?>
-              <?php if (count($active_filters) > 0): ?>
-                <span><?php print format_plural(count($active_filters), '1 selected subject', '@count selected subjects'); ?></span>
-              <?php else: ?>
-                <span><?php print t('Filter on subjects') ?></span>
-              <?php endif ?>
-              <img class="search-filters--icon-dropdown" src="/profiles/loopdk/themes/loop/images/nav-arrow-down-icon.png">
-            </span>
-          </span>
-
-          <div class="search-filters--subjects-wrapper">
-            <div class="search-filters--subjects">
-              <div class="search-filters--arrow"><div class="search-filters--arrow-inner"></div></div>
-              <div class="search-filters--subjects-inner">
-                <?php foreach ($form['#facets']['field_subject:name'] as $index => $facet): ?>
-                  <?php $key = 'field_subject:name-'.$index; ?>
-                  <?php $name = 'f[]'; ?>
-                  <?php $text = $facet['filter']; ?>
-                  <?php $value = urlencode('field_subject:name').':'.$text; ?>
-                  <?php $checked = $facet['active'] ?? FALSE; ?>
-
-                  <div class="search-filters--subjects-item">
-                    <span class="search-filters--subjects-item-inner">
-                      <input id="<?php print $key; ?>" type="checkbox" name="<?php print $name; ?>" <?php if ($checked) { print ' checked '; } ?> value="<?php print htmlspecialchars($value) ?>"/>
-                      <label for="<?php print $key; ?>"><?php print $text; ?></label>
-                    </span>
-                  </div>
-                <?php endforeach ?>
-              </div>
-              <span class="search-filters--action"><?php print t('Filter') ?></span>
+                <label class="search-filters--label <?php print $checked ? ' is-active' : ''; ?>"><?php print $facet['text'] ?? $facet['filter']; ?>
+                  <input type="checkbox" name="<?php print $name; ?>" <?php print $checked ? ' checked ' : ''; ?> value="<?php print htmlspecialchars($value) ?>"/>
+                </label>
+              <?php endforeach ?>
             </div>
-          </div>
-        <?php endif ?>
-      </div>
+          <?php endif ?>
+
+          <?php if (isset($form['#facets']['field_subject:name'])): ?>
+            <span class="search-filters--subject-wrapper">
+              <span class="search-filters--subject-wrapper-label">
+                <?php $active_filters = loop_search_db_get_active_filters('field_subject'); ?>
+                <?php if (count($active_filters) > 0): ?>
+                  <span><?php print format_plural(count($active_filters), '1 selected subject', '@count selected subjects'); ?></span>
+                <?php else: ?>
+                  <span><?php print t('Filter on subjects') ?></span>
+                <?php endif ?>
+                <img class="search-filters--icon-dropdown" src="/profiles/loopdk/themes/loop/images/nav-arrow-down-icon.png">
+              </span>
+            </span>
+
+            <div class="search-filters--subjects-wrapper">
+              <div class="search-filters--subjects">
+                <div class="search-filters--arrow"><div class="search-filters--arrow-inner"></div></div>
+                <div class="search-filters--subjects-inner">
+                  <?php // @TODO We should propably use some theme function for this, right? ?>
+                  <?php foreach ($form['#facets']['field_subject:name'] as $index => $facet): ?>
+                    <?php $key = 'field_subject:name-' . $index; ?>
+                    <?php $name = 'f[]'; ?>
+                    <?php $text = $facet['filter']; ?>
+                    <?php $value = urlencode('field_subject:name') . ':' . $text; ?>
+                    <?php $checked = $facet['active'] ?? FALSE; ?>
+
+                    <div class="search-filters--subjects-item">
+                      <span class="search-filters--subjects-item-inner">
+                        <input id="<?php print $key; ?>" type="checkbox" name="<?php print $name; ?>" <?php print $checked ? ' checked ' : ''; ?> value="<?php print htmlspecialchars($value) ?>"/>
+                        <label for="<?php print $key; ?>"><?php print $text; ?></label>
+                      </span>
+                    </div>
+                  <?php endforeach ?>
+                </div>
+                <span class="search-filters--action"><?php print t('Filter') ?></span>
+              </div>
+            </div>
+          <?php endif ?>
+        </div>
+      <?php endif ?>
 
 <?php if(0===1): ?>
       <div class="search-filters--sort">
